97615bfd4688eb3b5804dfd5e51181355dc440a5
Joe Cox <joeyalancox@gmail.com>
2016-03-02 17:15:12 -0800

    Rename class of MVarAsgnInst

--- src/petablox/analyses/inst/RelMVarAsgnInst.java
+++ src/petablox/analyses/inst/RelMVarAsgnInst.java
@@ -5,6 +5,8 @@ import soot.SootClass;
 import soot.SootMethod;
 import soot.Unit;
 import soot.Value;
+import soot.jimple.Constant;
+import soot.jimple.internal.JNewExpr;
 import soot.jimple.internal.JAssignStmt;
 import soot.jimple.internal.JCastExpr;
 import soot.shimple.internal.SPhiExpr;
@@ -12,6 +14,7 @@ import soot.toolkits.scalar.ValueUnitPair;
 
 import java.util.List;
 
+import petablox.program.visitors.IInstVisitor;
 import petablox.program.visitors.ICastInstVisitor;
 import petablox.program.visitors.IMoveInstVisitor;
 import petablox.program.visitors.IPhiInstVisitor;
@@ -29,7 +32,7 @@ import petablox.util.soot.SootUtilities;
  * @author Joe Cox (cox@cs.ucla.edu)
  */
 @Petablox(name = "MVarAsgnInst", sign = "M0,X0,X1:M0_X0xX1")
-public class RelMobjVarAsgnInst extends ProgramRel implements IMoveInstVisitor, IPhiInstVisitor, ICastInstVisitor {
+public class RelMVarAsgnInst extends ProgramRel implements IMoveInstVisitor, IPhiInstVisitor, ICastInstVisitor {
     private SootMethod ctnrMethod;
     public void visit(SootClass c) { }
     public void visit(SootMethod m) {
